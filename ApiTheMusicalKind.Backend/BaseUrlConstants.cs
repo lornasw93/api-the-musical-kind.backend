d170bcdb5b93270c89e6e73151f7f00f683266f3
Lorna <lorna.watson@epiphany.co.uk>
2020-08-05 09:48:26 +0100

    update api base url

--- ApiTheMusicalKind.Backend/BaseUrlConstants.cs
+++ ApiTheMusicalKind.Backend/BaseUrlConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class BaseUrlConstant
     {
-        public static string DeezerApi = "https://billboard-api2.p.rapidapi.com/";
+        public static string DeezerApi = "https://deezerdevs-deezer.p.rapidapi.com/";
         public static string LyricsOvh = "https://api.lyrics.ovh/v1/";
     }
 
